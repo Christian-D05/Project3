1850754b3cbe270913a2f68547fcdd9db0a122d8
Christian Domingo <cdomm14@gmail.com>
2025-12-02 23:15:52 -0500

    debugging

--- include/enlarger.h
+++ include/enlarger.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <utility>
 
-std::vector<std::pair<int, int>> read_edge_list(const string& filename);
+std::vector<std::pair<int, int>> read_edge_list(const std::string& filename);
 std::vector<std::pair<int, int>> enlarge_graph(const std::vector<std::pair<int, int>>& edges, int times, int bridges_per_pair);
 
 void write_edge_list(const std::string& filename, const std::vector<std::pair<int, int>>& edges);
